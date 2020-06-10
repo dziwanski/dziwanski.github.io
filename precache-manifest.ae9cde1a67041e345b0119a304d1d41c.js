0ccf526d6c8d6e060918b5ca9cd0ccfbe866bf37
dziwanski <anw93iw@gmail.com>
2020-06-10 10:25:05 +0200

    Updates

---
+++ precache-manifest.ae9cde1a67041e345b0119a304d1d41c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ee55b2eced75d816cc7103690582c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98c503ef7d80f5eb6a3f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ffd39493714998b4ef17",
+    "url": "/static/js/2.e208f2e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e208f2e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98c503ef7d80f5eb6a3f",
+    "url": "/static/js/main.a715186d.chunk.js"
+  },
+  {
+    "revision": "4f551c8d01fb1d584a21",
+    "url": "/static/js/runtime-main.4974d627.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
